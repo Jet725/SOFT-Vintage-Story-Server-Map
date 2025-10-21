f1326340647c334b4ad93a263b9a5fd809d2167e
Jet725 <83094388+Jet725@users.noreply.github.com>
2025-10-21 14:59:10 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ var settings = {
     title: "My VS Server",
     titleBarName: "My VS",
     titleBarCommunity: "My Community",
-    siteUrl: "https://example.com",
+    siteUrl: "https://jet725.github.io/SOFT-Vintage-Story-Server-Map/",
     updateText : "Last updated on ",
     lastUpdated : "Tuesday, 21 October 2025."
 }
