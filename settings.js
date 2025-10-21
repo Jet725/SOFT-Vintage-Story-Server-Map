1b60618679b2f308515351119e0b69b85be984e5
Jet725 <83094388+Jet725@users.noreply.github.com>
2025-10-21 14:52:17 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,8 @@
+var settings = {
+    title: "My VS Server",
+    titleBarName: "My VS",
+    titleBarCommunity: "My Community",
+    siteUrl: "https://example.com",
+    updateText : "Last updated on ",
+    lastUpdated : "Tuesday, 21 October 2025."
+}
