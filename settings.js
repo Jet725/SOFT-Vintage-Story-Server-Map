5ce2ad37e64cf245025d4b88f49f97d220aac3c0
Jet725 <Jetpack725@gmail.com>
2025-10-22 13:19:17 -0400

    Landmark Fix

--- settings.js
+++ settings.js
@@ -4,5 +4,5 @@ var settings = {
     titleBarCommunity: "WIP",
     siteUrl: "https://jet725.github.io/SOFT-Vintage-Story-Server-Map/",
     updateText : "Last updated on ",
-    lastUpdated : "Tuesday, 21 October 2025."
+    lastUpdated : "Wednesday, 22 October 2025."
 }
