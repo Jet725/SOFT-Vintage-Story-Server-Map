550e55e2bb2e2eb834292e755b713da08fb6b569
Jet725 <Jetpack725@gmail.com>
2025-10-24 11:01:58 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,5 +4,5 @@ var settings = {
     titleBarCommunity: "WIP",
     siteUrl: "https://jet725.github.io/SOFT-Vintage-Story-Server-Map/",
     updateText : "Last updated on",
-    lastUpdated : "Thursday, 23 October 2025."
+    lastUpdated : "Friday, 24 October 2025."
 }
